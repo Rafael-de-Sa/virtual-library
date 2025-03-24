e6775c5aa2ff22f4958d742798b1d36c06321948
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-03-24 20:24:52 -0300

    ✨ feat: save data into user table

--- cadastrar.php
+++ cadastrar.php
@@ -1,10 +1,18 @@
 <?php
-
+require_once("conexao.php");
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$password2 = $_POST['password2'];
 
-echo ($name);
-echo ($email);
-echo ($password);
+if ($password === $password2) {
+    $sql = "INSERT INTO usuario (nome, email, senha) VALUES ('$name', '$email', '$password')";
+    if (mysqli_query($conn, $sql)) {
+        echo "Usuário cadastrado com sucesso";
+        header("refresh;5;url=index.php");
+    }
+} else {
+    echo "As senhas não conferem";
+    header("refresh;5;url=index.php");
+}
